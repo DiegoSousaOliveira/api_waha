7fcfb1a8cb9df9eb71f6b0a0c32d4b78288e2c0f
Diego de Sousa Oliveira <diegosousa0056@gmail.com>
2025-02-13 21:54:57 -0300

    arquivo que recebe e enviar mensagem para o whatsapp.

---
+++ services/waha.py
@@ -0,0 +1,55 @@
+import requests
+
+class Waha:
+    
+    def __init__(self) -> None:
+        self.__url = 'http://waha:3000'
+
+    def send_message(self, chat_id, message):
+        url = f'{self.__url}/api/sendText'
+
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        payload = {
+            'session':   'default',
+            'chatId':    chat_id,
+            'text':      message
+        }
+        requests.post(
+            url         =  url,
+            json        =  payload,
+            headers     =  headers
+        )
+
+    def start_typing(self, chat_id):
+        url = f'{self.__url}/api/startTyping'
+
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        payload = {
+            'session': 'default',
+            'chatId':   chat_id
+        }
+        requests.post(
+            url         =  url,
+            json        =  payload,
+            headers     =  headers
+        )
+    
+    def stop_typing(self, chat_id):
+        url = f'{self.__url}/api/stopTyping'
+
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        payload = {
+            'session': 'default',
+            'chatId': chat_id,
+        }
+        requests.post(
+            url         =  url,
+            json        =  payload,
+            headers     =  headers
+        )
